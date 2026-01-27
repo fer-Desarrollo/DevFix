5b78fe54640f2f7ddb9ccea093ebbdb3a079f385
Fernando Gracia Torres <fernandoGT.Desarrollo@hotmail.com>
2026-01-26 20:19:02 -0600

    first commit

---
+++ src/main/java/com/DevFiX/DevFixApplication.java
@@ -0,0 +1,13 @@
+package com.DevFiX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevFixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevFixApplication.class, args);
+	}
+
+}
